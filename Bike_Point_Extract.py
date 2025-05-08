10de565cae7a9ac1b92dc12d4b3e5739ed49feb4
AshaDaniels <asha.m.daniels@gmail.com>
2025-05-08 16:38:56 +0100

    Added in for loop to extract all ids

--- Bike_Point_Extract.py
+++ Bike_Point_Extract.py
@@ -1,26 +1,36 @@
 import requests
 import json
+import os
 
-url = 'https://api.tfl.gov.uk/BikePoint/BikePoints_1'
-id = 'BikePoints_1'
-
+url = 'https://api.tfl.gov.uk/BikePoint'
 response = requests.get(url)
 
 if response.status_code == 200:
     #save the json response into a data variable
     data = response.json()
+    id = [item.get('id') for item in data]
 
-    #get modified time stamp to help us name our file
-    first_value = data['additionalProperties'][0]
-    modified = first_value.get('modified')
-    modified = modified.replace(":", "-")
-    modified = modified.replace(".", "-")
+    for bp in id: 
+        endpoint = url + '/' + bp
+        endpoint_response = requests.get(endpoint)
+        endpoint_data = endpoint_response.json()
 
-    filename = modified + id + '.json'
+        #get modified time stamp to help us name our file
+        first_value = endpoint_data['additionalProperties'][0]
+        modified = first_value.get('modified')
+        modified = modified.replace(":", "-")
+        modified = modified.replace(".", "-")
 
-    with open(filename, 'w') as file:
-        json.dump(data,file)
-    
+        file_list = [f for f in os.listdir('.') if f.endswith('.json')]
+        
+        filename = modified + bp + '.json'
+
+        if filename in file_list:
+            print('Up to date')
+        else:
+            with open(filename, 'w') as file:
+                json.dump(endpoint_data,file)
+        
 else:
     # Returns a cleaner error message
     data = response.json()
