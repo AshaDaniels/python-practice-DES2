8e8f2056605926be2b78d3d6117bd70714a67c72
AshaDaniels <asha.m.daniels@gmail.com>
2025-05-13 15:00:37 +0100

    Changed to only call the API once

--- Bike_Point_Extract.py
+++ Bike_Point_Extract.py
@@ -1,21 +1,25 @@
+# import Libraries
 import requests
 import json
 import os
 
+# call  API
 url = 'https://api.tfl.gov.uk/BikePoint'
 response = requests.get(url)
 
 if response.status_code == 200:
-    #save the json response into a data variable
+    # if the response is successful, save the json response into a data variable
     data = response.json()
+    # extract a list of id values for all bikes
     id = [item.get('id') for item in data]
 
+    # iterate through the ids and request individual bike data
     for bp in id: 
         endpoint = url + '/' + bp
         endpoint_response = requests.get(endpoint)
         endpoint_data = endpoint_response.json()
 
-        #get modified time stamp to help us name our file
+        # get modified time stamp to name our file
         first_value = endpoint_data['additionalProperties'][0]
         modified = first_value.get('modified')
         modified = modified.replace(":", "-")
@@ -25,14 +29,16 @@ if response.status_code == 200:
         
         filename = modified + bp + '.json'
 
+        # if the file already exists, just print an up to date message
         if filename in file_list:
             print('Up to date')
         else:
+        # otherwise create the file
             with open(filename, 'w') as file:
                 json.dump(endpoint_data,file)
         
 else:
-    # Returns a cleaner error message
+    # error handling
     data = response.json()
     error_message = data.get("message", "No message provided.")
     print(f'Error {response.status_code}:{error_message}')
---
+++ all_bike_points.py
@@ -0,0 +1,49 @@
+# Import Libraries
+import requests  # For making HTTP requests to fetch data from APIs
+import json  # For working with JSON data
+import os  # For interacting with the operating system, e.g., file handling
+
+# Call the API
+url = 'https://api.tfl.gov.uk/BikePoint'  # URL of the API endpoint to fetch bike point data
+response = requests.get(url)  # Send a GET request to the API
+
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()  # Parse the JSON response into a Python dictionary
+
+    # Count the number of bike points
+    number_of_bike_points = len([item.get('id') for item in data])
+
+    # Iterate through each bike point
+    for i in range(0, number_of_bike_points):
+        bike_point = data[i]  # Get the bike point data for the current index
+
+        # Get the first additional property and process its 'modified' value
+        first_value = bike_point['additionalProperties'][0]  # Access the first additional property
+        modified = first_value.get('modified')  # Retrieve the 'modified' timestamp
+        # Replace problematic characters in the 'modified' value to make it file-system friendly
+        modified = modified.replace(":", "-")
+        modified = modified.replace(".", "-")
+
+        # Get a list of all JSON files in the current directory
+        file_list = [f for f in os.listdir('.') if f.endswith('.json')]
+
+        # Get the bike point's ID
+        bp = bike_point.get('id')
+        
+        # Create a filename using the modified timestamp and bike point ID
+        filename = modified + bp + '.json'
+
+        # Check if the file already exists in the directory
+        if filename in file_list:
+            print('Up to date')  # Indicate the file is up to date
+        else:
+            # Otherwise, create the file
+            with open(filename, 'w') as file:
+                json.dump(bike_point, file)  # Write the bike point data to a JSON file
+
+else:
+    # Handle errors if the API request was not successful
+    data = response.json()  # Parse the JSON response
+    error_message = data.get("message", "No message provided.")  # Get the error message
+    print(f'Error {response.status_code}:{error_message}')  # Print the error code and message
